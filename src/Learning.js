92ab6ef6fe1aad832f35c170f9a1953a371bf48a
AMAN PREET SINGH <amansainigahonia@gmail.com>
2022-11-08 12:06:14 -0600

    aman

--- src/Learning.js
+++ src/Learning.js
@@ -16,7 +16,7 @@ function Learning() {
         <a className='Link' href='https://github.com/Aman-1313'>GitHub<GitHubIcon /></a>
       </div>
       <div className='Body'>
-        <img class="rocket launch" src="https://cdn4.iconfinder.com/data/icons/whsr-january-flaticon-set/512/rocket.png" alt='ROCKET' />
+        <img className="rocket" src="https://cdn4.iconfinder.com/data/icons/whsr-january-flaticon-set/512/rocket.png" alt='ROCKET' />
         <p className='Quote'>The only way to rectify our reasonings is to make them as tangible as those of the mathematicians, so that we can find our error at a glance, and when there are disputes among persons we can simply say, “Let us calculate... to see who is right.”—Leibniz, The Art of Discovery</p>
           <Grid container spacing={2}>
             <Grid item xs={6}>
